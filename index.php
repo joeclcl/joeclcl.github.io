8b32e92dd650ae4e696640bda37a9b099738fd48
joeclcl <93515628+joeclcl@users.noreply.github.com>
2022-03-13 00:53:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,17 @@
   $vist_page     =   "index.php";
   include "logger.php";
   ?>
+
+<html>
+  <body style="background-color:black;">
+  <center>
+    <h1 style="color: yellow">Joe's Cult Index Page</h1>
+    <a href="http://joes-cult.tk/lock.html" style="color: yellow">"password"<br></a>    
+    <a href="http://joes-cult.tk/f2aa58e18c229968adaf77d71e8cd9f65a4a55fc/index.html" style="color: yellow">pricing<br></a>    
+    <a href="http://joes-cult.tk/72017e335e33e9d15fac3c5c0d3696b397e48b71/index.html" style="color: yellow">dangrips<br></a>
+    <a href="http://joes-cult.tk/fe12d0f6d9001004ee315e7219869324ecb8ab69/index.html" style="color: yellow">tomjonesrip<br></a>
+    <a href="http://joes-cult.tk/fef3343992105081ef53cb02df4f3890813e19e9/index.html" style="color: yellow">crisps<br></a>
+    <h3 style="color: yellow">more coming soon</h3>
+  </center>
+</body>
+</html>
